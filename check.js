482d3168aa53d44dd59116ce434d613091888e65
Divyanshkaushal <dkaushal.enabke@gmail.com>
2018-08-02 19:20:19 +0530

    changes done

--- check.js
+++ check.js
@@ -1,6 +1,19 @@
+// $(document).ready(function(){
+//     $('input[type="checkbox"]').click(function(){
+//         var inputValue = $(this).attr("value");
+//         $("." + inputValue).toggle();
+//     });
+// });
+
 $(document).ready(function(){
-    $('input[type="checkbox"]').click(function(){
-        var inputValue = $(this).attr("value");
-        $("." + inputValue).toggle();
-    });
+	$('.chkbox').click(function(){
+		var text = "" ;
+		$('.chkbox:checked').each(function()
+		{
+			text+=$(this).val()+ ',';
+		});
+		// text=text.substring(0,text.length-1);
+		$("#selectedtext").val(text + " is selected");
+		$('#count').val($("[type='checkbox']:checked").length);
+	});
 });
